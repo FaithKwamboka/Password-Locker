df149f60dd6042b1e4b64db701df963f0d72fed5
FaithKwamboka <okongofaith3@gmail.com>
2022-04-24 02:52:32 +0300

    Adding more functionalities to the login function

--- login.py
+++ login.py
@@ -59,3 +59,51 @@ def Login():
                                                         
                                     print("password")
                                     user_password= input()
+
+                                    run.save_credentials(run.create_credentials(accountName,user_name,user_password))
+                                    print("\n")
+                                    print(f"New {accountName} credentials created and saved:")
+                                    print(f"Name:{user_name}")
+                                    print(f"password:{user_password}")
+                                    print("\n")
+                            else:
+                                print("*"*60)
+                                print("Account name eg twitter")
+                                accountName= input()
+                                    
+                            
+                                print("User Name")
+                                user_name= input()                  
+                            
+                                user_password=generate_password()
+                                print("*"*60)
+                                run.save_credentials(run.create_credentials(accountName,user_name,user_password))
+                                print("\n")
+                                print(f"New {accountName} credentials created and saved:")
+                                print(f"Name:{user_name}")
+                                print(f"password:{user_password}")
+                                print("\n")
+                                print("*"*60)
+            elif user_input == "dc":
+                if run.display_credentials():
+                    print("Here is a list of your saved passwords")
+                    print("\n")
+                    
+                    for credential in run.display_credentials():
+                        print(f"{credential.username} {credential.account_name} ......{credential.userpassword}")
+                    print('\n')
+                else:
+                    print('\n')
+                    print("Sorry! You do not have any saved passwords")
+                    print('\n')
+                    
+            elif user_input == "dd":
+                print("Enter the account you want to delete:" )
+                deleteAccount = input()
+                print(f"{deleteAccount} account successfuly deleted")
+                
+            elif user_input == "ex":
+                            print("Bye for now.......")
+                            break
+            else:
+                            print("I really didn't get that. Please use the short codes") 
