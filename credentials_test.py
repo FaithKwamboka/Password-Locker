a669997400c3079e170134893826f6df744666dc
FaithKwamboka <okongofaith3@gmail.com>
2022-04-23 14:22:08 +0300

    Testing Credentials

--- credentials_test.py
+++ credentials_test.py
@@ -4,7 +4,7 @@ from credentials import Credential
 
 class TestCredentials(unittest.TestCase):
     """
-    Test class that defines test cases for the contact class behaviours.
+    Test class that defines test cases for the credentials class behaviours.
 
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
