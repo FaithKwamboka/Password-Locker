d96cf61e1dd123296ab7c21b16a350f85cf9b039
FaithKwamboka <okongofaith3@gmail.com>
2022-04-23 11:07:33 +0300

    Testing Credentials

---
+++ credentials_test.py
@@ -0,0 +1,29 @@
+import unittest
+from credentials import Credential
+
+
+class TestCredentials(unittest.TestCase):
+    """
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    """
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = Credential("Facebook", "Faith", "Kwamboka@")
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.account_name, "Facebook")
+        self.assertEqual(self.new_user.username, "Faith")
+        self.assertEqual(self.new_user.password, "Kwamboka@")
+
+
+if __name__ == '__main__':
+    unittest.main()
