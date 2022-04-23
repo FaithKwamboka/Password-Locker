02cfa98830621ee6eaf0fa6908110d9cf6da202a
FaithKwamboka <okongofaith3@gmail.com>
2022-04-23 10:52:12 +0300

    Defining find account, account exists and display class methods

--- credentials.py
+++ credentials.py
@@ -10,3 +10,37 @@ class Credential:
         self.password = password
 
         credentials_list = []
+
+    def save_credential(self):
+        """
+        This method saves user objects to the users list
+        """
+        self.new_credential.save_credential()
+        Credential.credentials_list.append(self)
+        
+    def delete_credential(self):
+        """
+        This method deletes user objects from the users list
+        """
+        Credential.credentials_list.remove(self)
+
+    @classmethod
+    def find_by_account(cls,account_name):
+    
+        for credential in cls.credentials_list:
+            if credential.account_name == account_name:
+                return credential
+    
+    @classmethod
+    def account_exists(cls,account_name):
+        for credential in cls.credentials_list:
+            if credential.account_name == account_name:
+                return True
+        return False
+
+    # display Credential
+    @classmethod
+    def display_credential(cls):
+        return cls.credentials_list
+
+        
